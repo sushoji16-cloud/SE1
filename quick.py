f9a2a85051036bad9cae849f79625cc494270bf0
sushoji16-cloud <sushoji16@gmail.com>
2026-03-27 06:20:56 +0530

    Update quick.py

--- quick.py
+++ quick.py
@@ -1,22 +1,35 @@
 def quick_sort(arr):
-    if arr == []:
-        return []
+                if arr == []: 
+            return []
 
-    pivot = arr[-1]   # choose last element as pivot
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+pivot = arr[-1]   # choose last element as pivot
     list1 = []        # elements <= pivot
     list2 = []        # elements > pivot
-
-    for item in arr[:-1]:
+                for item in arr[:-1]:
         
 if item > pivot:
                    list2.append(item)
         else:
-            list1.append(item)
+    list1.append(item)
 
-               return quick_sort(list1) + [pivot] + quick_sort(list2)
+                  return quick_sort(list1) + [pivot] + quick_sort(list2)
 
 
-inp = input("Enter the list numbers separated by a space: ").split()
+inp =     input("Enter the list numbers separated by a space: ").split()
 arr = [int(x) for x in inp]
 
 print("Unsorted list:", arr)
