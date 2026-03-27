aa3ad611bc7927d0fef1ddc9ef2fd6519216fcde
sushoji16-cloud <sushoji16@gmail.com>
2026-03-27 06:25:42 +0530

    Update quick.py

--- quick.py
+++ quick.py
@@ -1,36 +1,28 @@
-def quick_sort(arr):
-                if arr == []: 
-            return []
 
-   
+# BAD CODE (for SonarQube testing)
 
+import os
 
+password = "admin123"   # Hardcoded password (SECURITY ISSUE)
 
+def calculate(a, b):
+    if a == b:
+        print("Equal")   # Code smell (useless condition)
+    return a / b   # Possible division by zero (RELIABILITY ISSUE)
 
+def execute_command(user_input):
+    os.system("echo " + user_input)   # Command injection (SECURITY ISSUE)
 
+def process_list(lst):
+    for i in range(len(lst)):   # Bad practice (MAINTAINABILITY)
+        print(lst[i])
 
+def unused_function():
+    x = 10   # Unused variable (CODE SMELL)
+    return
 
+# No exception handling anywhere (RELIABILITY ISSUE)
 
-
-
-
-
-
-pivot = arr[-1]   # choose last element as pivot
-    list1 = []        # elements <= pivot
-    list2 = []        # elements > pivot
-                for item in arr[:-1]:
-        
-if item > pivot:
-                   list2.append(item)
-        else:
-    list1.append(item)
-
-                  return quick_sort(list1) + [pivot] + quick_sort(list2)
-
-
-inp =     input("Enter the list numbers separated by a space: ").split()
-arr = [int(x) for x in inp]
-
-print("Unsorted list:", arr)
-print("Sorted list is:", quick_sort(arr))
+print(calculate(10, 0))   # Will crash
+execute_command("Hello")
+process_list([1, 2, 3])
